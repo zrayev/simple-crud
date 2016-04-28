3f729d48ac66da6971d4802e27e96ef20e9f282a
dmitry <zrayev@mail.ru>
2016-04-28 20:52:11 +0300

    init poject

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,3 +1,4 @@
 <?php
 
-$pdo = new PDO('mysql:host=127.0.0.1;port=3308;dbname=crud;charset=utf8', 'crud', 'crud');
+$pdo = new PDO('mysql:host=127.0.0.1;port=3306;dbname=simple_crud;charset=utf8', 'crud', 'crud');
+
