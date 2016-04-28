be5651ec5ecb330cbbe43ef064468e4ff0c0c3cf
Maxim Polishchuck <mpolishchuck@gmail.com>
2016-04-28 19:35:05 +0300

    fixed update

--- op/update.php
+++ op/update.php
@@ -6,16 +6,19 @@ $sth->execute(array(':id' => $_GET['id']));
 if ($repo = $sth->fetch(PDO::FETCH_ASSOC)) {
 
   if (!empty($_POST['save'])) {
-    $sth = $pdo->prepare('UPDATE repositories SET name = :name, active = :active WHERE id = :id');
+    $sth = $pdo->prepare('UPDATE repositories SET name = :name, active = :active, id_storage = :id_storage WHERE id = :id');
     $sth->execute(array(
       ':name' => $_POST['name'],
       ':active' => (!empty($_POST['active']) ? 1 : 0),
+      ':id_storage' => $_POST['id_storage'],
       ':id' => $_GET['id'],
     ));
 
     header('Location: index.php');
     exit;
   }
+
+  $storages = get_available_storages();
 
   ob_start();
 
